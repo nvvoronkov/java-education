316b459420bb0d527e4aab660cbbf63a6acd988d
Воронков Никита <nikitavoronkov@yandex.ru>
2023-08-25 14:34:07 +0300

    six lesson

---
+++ src/main/java/lesson/six/Course.java
@@ -0,0 +1,4 @@
+package lesson.six;
+
+public interface Course {
+}
--- src/main/java/lesson/six/Main.java
+++ src/main/java/lesson/six/Main.java
@@ -1,7 +1,29 @@
 package lesson.six;
 
+import java.util.Comparator;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+    }
 
+    public List<Course> getUniqueCourses(List<Student> students) {
+        return students.stream()
+                .flatMap(student -> student.getAllCourses().stream())
+                .distinct()
+                .toList();
+    }
+
+    public List<Student> getMostCurious(List<Student> students) {
+        return students.stream()
+                .sorted(Comparator.comparing(student -> student.getAllCourses().size()))
+                .limit(3)
+                .toList();
+    }
+
+    public List<Student> getStudentsByCourse(List<Student> students, Course course) {
+        return students.stream()
+                .filter(student -> student.getAllCourses().contains(course))
+                .toList();
     }
 }
